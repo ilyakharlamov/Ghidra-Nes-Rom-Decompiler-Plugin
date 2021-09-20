193b03b8dddcc0217c8118e9f28a57d822a1c065
ikharlamov <502372+ilyakharlamov@users.noreply.github.com>
2021-09-20 03:45:06 +0000

    6502NES processor

---
+++ ghidra_scripts/src/Test6502.java
@@ -0,0 +1,15 @@
+package ik.ghidranesrom.script;
+//TODO test your ideas fast here
+//@author Ilya Kharlamov
+//@category _NEW_
+//@keybinding
+//@menupath Tools.NES
+//@toolbar
+
+import ghidra.app.script.GhidraScript;
+
+public class Test6502 extends GhidraScript {
+    public void run() throws Exception {
+        println("test");
+    }
+}
